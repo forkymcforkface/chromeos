cb8957937c5614b1011dc61c7b1801a2869d961d
Kroese <kroese@users.noreply.github.com>
2026-07-13 21:20:22 +0200

    Create entry.sh

---
+++ src/entry.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+: "${APP:="ChromeOSFlex"}"
+: "${SHUTDOWN:="Y"}"
+: "${TIMEOUT:="60"}"
+: "${PLATFORM:="x64"}"
+: "${SUPPORT:="https://github.com/forkymcforkface/chromeos"}"
+
+cd /run
+
+. start.sh      # Startup hook
+. utils.sh      # Load functions
+. reset.sh      # Initialize system
+. server.sh     # Start webserver
+. install.sh    # Download the image
+. disk.sh       # Initialize disks
+. display.sh    # Initialize graphics
+. network.sh    # Initialize network
+. boot.sh       # Configure boot
+. proc.sh       # Initialize processor
+. power.sh      # Configure shutdown
+. memory.sh     # Check available memory
+. balloon.sh    # Initialize ballooning
+. config.sh     # Configure arguments
+. finish.sh     # Finish initialization
+
+trap - ERR
+
+cmd=(qemu-system-x86_64)
+version=$("${cmd[@]}" --version | awk 'NR==1 { print $4 }')
+info "Booting $APP using QEMU v$version..." && echo
+
+if [[ "$SHUTDOWN" != [Yy1]* ]]; then
+  exec "${cmd[@]}" ${ARGS:+ $ARGS}
+fi
+
+"${cmd[@]}" ${ARGS:+ $ARGS} &
+
+rc=0
+wait $! || rc=$?
+[ -f "$QEMU_END" ] && exit "$rc"
+
+sleep 1 & wait $!
+finish "$rc"
