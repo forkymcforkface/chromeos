8a3bf07abacf2ec433c0b521c8d1e83e15fd920e
Kroese <kroese@users.noreply.github.com>
2026-08-06 22:58:40 +0200

    Update entrypoint

--- src/entry.sh
+++ src/entry.sh
@@ -16,7 +16,8 @@ cd /run
 
 . start.sh      # Startup hook
 . utils.sh      # Load functions
-. reset.sh      # Initialize system
+. init.sh       # Initialize system
+. memory.sh     # Check memory
 . server.sh     # Start webserver
 . define.sh     # ChromeOS parameters
 . install.sh    # Download the image
@@ -27,7 +28,6 @@ cd /run
 . boot.sh       # Configure boot
 . proc.sh       # Initialize processor
 . power.sh      # Configure shutdown
-. memory.sh     # Check available memory
 . balloon.sh    # Initialize ballooning
 . config.sh     # Configure arguments
 . finish.sh     # Finish initialization
